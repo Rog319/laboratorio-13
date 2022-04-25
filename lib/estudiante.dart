b71d642b84dc33de5b35dbf85ea3392e97584c5d
Rog319 <al18760466@ite.edu.mx>
2022-04-25 16:42:28 -0700

    Creacion del proyecto

---
+++ lib/estudiante.dart
@@ -0,0 +1,11 @@
+class Estudiante {
+  String matricula;
+  String nombreCompleto;
+  String carrera;
+  String semestre;
+  String telefono;
+  String correo;
+
+  Estudiante(this.matricula, this.nombreCompleto, this.carrera, this.semestre,
+      this.telefono, this.correo);
+}
